6fd4b80c61ab647568af3455e5d9b29ad241eaa0
Christian M <chriamue@gmail.com>
2020-05-07 07:38:51 +0200

    :pencil: :white_check_mark: updated diary test and added badges to readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # c-diary-js
 Corona Diary Lib
+[![Build Status](https://travis-ci.org/c-diary/c-diary-js.svg?branch=master)](https://travis-ci.org/c-diary/c-diary-js)
+[![Coverage Status](https://coveralls.io/repos/github/c-diary/c-diary-js/badge.svg?branch=master)](https://coveralls.io/github/c-diary/c-diary-js?branch=master)
 
 ## Getting started
 
--- src/tests/diary.test.ts
+++ src/tests/diary.test.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import Diary, { fromJson } from '../diary';
 import DiaryEntry from '../diaryEntry';
 
@@ -21,4 +22,23 @@ test('symptoms', () => {
     diary.addEntry(entry2);
     diary.addEntry(entry3);
     expect(diary.getSymptoms()).toEqual(['cough', 'fever'])
+})
+
+test('sorted entries', () => {
+    const current = moment(new Date());
+    const entry1 = new DiaryEntry(1, current.toDate());
+    entry1.addSymptoms(['cough']);
+    const entry2 = new DiaryEntry(1, current.add(1, 'seconds').toDate());
+    entry2.addSymptoms(['fever', 'cough']);
+    const entry3 = new DiaryEntry(1, current.add(1, 'seconds').toDate());
+    entry3.addSymptoms(['fever']);
+    const diary = new Diary();
+    diary.addEntry(entry3);
+    diary.addEntry(entry1);
+    diary.addEntry(entry2);
+    const entries = diary.sortedEntries();
+    expect(entries[0]).toBe(entry1);
+    expect(entries[1]).toBe(entry2);
+    expect(entries[2]).toBe(entry3);
+
 })
